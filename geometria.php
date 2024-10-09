8aa6f8a0aca0051d69da3df921c314bc38061c1c
DragonautMk <michelesavarese1997@gmail.com>
2024-10-09 09:44:52 +0200

    esercizio 4

--- geometria.php
+++ geometria.php
@@ -24,9 +24,63 @@ Area → b * h;
 Perimetro → 2 * b + 2 * h;
 Diagonale → √ hˆ2 + bˆ2 (Tutto sotto radice)
 Per fare questi calcoli dovete sfruttare le funzioni del Trait Calculator
- extra: provate a creare classi per altre figure geometriche e calcolare area e perimetro anche lì sempre sfruttando il trait
+extra: provate a creare classi per altre figure geometriche e calcolare area e perimetro anche lì sempre sfruttando il trait
 
 
- <?php
+<?php
 
- ?>
+trait Calculator {
+  public function sum($a, $b) {
+    return $a + $b;
+  }
+  public function sub($a, $b) {
+    return $a - $b;
+  }
+  public function mul($a, $b) {
+    return $a * $b;
+  }
+  public function div($a, $b) {
+    return $a / $b;
+  }
+  public function sqr($a){
+    return sqrt($a);
+  }
+}
+
+class Rectangle{
+  use Calculator;
+
+  public $height;
+  public $base;
+
+  public function __construct($height,$base)
+  {
+    $this->height = $height;
+    $this->base = $base;
+  }
+
+  public function area(){
+    return "L'area del rettangolo è: {$this->mul($this->base, $this->height)}";
+}
+public function perimetro(){
+    return "Il perimetro del rettangolo è: {$this->sum($this->mul($this->base, 2),$this->mul($this->height,2))}";
+}
+
+public function diagonale(){
+    return "La diagonale del rettangolo è: {$this->sqr($this->sum($this->mul($this->base, $this->base), $this->mul($this->height, $this->height)))}";
+}
+public function info()
+{
+    echo "
+    {$this->area()}
+    {$this->perimetro()}
+    {$this->diagonale()}
+    ";
+}
+
+}
+
+$rettangolo = new Rectangle(5,4);
+echo $rettangolo->info();
+
+?>
